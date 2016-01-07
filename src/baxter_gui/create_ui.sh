0454174b657b75c7d847d3e03530ad8fa4f7c8d2
Sven Bock <sb@generationrobots.com>
2016-01-07 18:26:11 +0100

    added an error message to the script to generate the python qt class

--- src/baxter_gui/create_ui.sh
+++ src/baxter_gui/create_ui.sh
@@ -1,1 +1,8 @@
-pyside-uic baxtergui.ui > baxtergui.py
+{ # this is my bash try block
+	
+	pyside-uic baxtergui.ui > baxtergui.py
+	echo "Regenerating baxtergui.py"
+} || { # this is catch block
+	echo "Install missing pyside tools by:"
+	echo "sudo apt-get install pyside-tools"
+}
